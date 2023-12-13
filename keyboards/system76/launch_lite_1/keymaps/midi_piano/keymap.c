ac9be4fc00ee526de75722af3dbc2acccd43a391
garrett wilke <garrettwilke@garretts-MacBook-Air.local>
2023-12-12 18:08:04 -0700

    fixed launch_lite_1

--- keyboards/system76/launch_lite_1/keymaps/midi_piano/keymap.c
+++ keyboards/system76/launch_lite_1/keymaps/midi_piano/keymap.c
@@ -31,7 +31,7 @@ ________________________________________________________________________________
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,  KC_PGUP,
           KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,    KC_ENT,       KC_PGDN,
           KC_LSFT,     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,    KC_RSFT,    KC_UP, KC_END,
-          KC_LCTL, KC_LALT, MO(1),   KC_LGUI,     KC_SPC,           KC_SPC,     KC_RCTL,   KC_RALT,   MO(1),         KC_LEFT, KC_DOWN, KC_RGHT
+          KC_LCTL, KC_LALT, MO(_FUNC),   KC_LGUI,     KC_SPC,           KC_SPC,     KC_RCTL,   KC_RALT,   MO(1),         KC_LEFT, KC_DOWN, KC_RGHT
   ),
 
 
@@ -40,15 +40,15 @@ ________________________________________________________________________________
        MI_C,     MI_D,   MI_E,    MI_F,   MI_G,   MI_A,   MI_B,   MI_C1,   MI_D1,   MI_E1,     MI_F1,  MI_G1,  MI_A1,    MI_B1, _______,
       _______,     MI_Cs2, MI_Ds2, _______, MI_Fs2, MI_Gs2, MI_As2, _______, MI_Cs3, MI_Ds3, _______, MI_Fs3, MI_Gs3,         _______,
              MI_C2,   MI_D2,  MI_E2,    MI_F2,  MI_G2,  MI_A2, MI_B2,    MI_C3,  MI_D3,  MI_E3,     MI_F3, MI_G3,          MI_OCTU, _______,
-      _______,   MO(_FUNC), _______, _______, _______,          _______,        _______, _______, _______,         _______, MI_OCTD, _______
+      _______,   _______, MO(_FUNC), _______, _______,          _______,        _______, _______, _______,         _______, MI_OCTD, _______
   ),
 
   [_PADS] = LAYOUT(
-    _______, MI_C, MI_Cs,  MI_D, MI_Ds,  _______, _______,   MI_E1, MI_F1, MI_Fs1, MI_G1, _______, _______,_______,    _______,
-    _______,   MI_E, MI_F, MI_Fs, MI_G, _______, _______,      MI_Gs1, MI_A1, MI_As1, MI_B1, _______, _______, _______,  _______,
-      _______,   MI_Gs, MI_A, MI_As, MI_B, _______, _______,     MI_C2, MI_Cs2, MI_D2, MI_Ds2, _______, _______,         _______,
-      _______,     MI_C1, MI_Cs1, MI_D1, MI_Ds1, _______, _______, MI_E2, MI_F2, MI_Fs2, MI_G2,  _______,       MI_OCTU, _______,
-      _______,   MO(_FUNC), _______, _______, _______,          _______,        _______, _______, _______,         _______, MI_OCTD, _______
+    _______, MI_C1, MI_Cs1,  MI_D1, MI_Ds1,  _______, _______,   MI_E2, MI_F2, MI_Fs2, MI_G2, _______, _______,_______,    _______,
+    _______,   MI_Gs, MI_A, MI_As, MI_B, _______, _______,      MI_C2, MI_Cs2, MI_D2, MI_Ds2, _______, _______, _______,  _______,
+      _______,   MI_E, MI_F, MI_Fs, MI_G, _______, _______,     MI_Gs1, MI_A1, MI_As1, MI_B1, _______, _______,         _______,
+      _______,     MI_C, MI_Cs, MI_D, MI_Ds, _______, _______, MI_E1, MI_F1, MI_Fs1, MI_G1,  _______,       MI_OCTU, _______,
+      _______,   _______, MO(_FUNC), _______, _______,          _______,        _______, _______, _______,         _______, MI_OCTD, _______
   ),
 
     /* Layer 1, function layer
@@ -75,7 +75,7 @@ ________________________________________________________________________________
     TO(_BASE), TO(_MIDI), TO(_PADS), _______, KC_R, _______, _______, _______, _______, KC_O,  _______, _______, _______, _______,              KC_VOLD,
       KC_CAPS,   KC_A, KC_S, _______, _______, _______, _______, _______, _______,   _______, _______, _______, KC_ENT,         MI_AOFF,
       KC_LSFT,     KC_Z, KC_X, KC_C, KC_V, _______, _______, _______, _______, _______, _______,       _______,           KC_UP, _______,
-      KC_LCTL,   _______, KC_LALT, KC_LGUI, KC_SPC,          KC_SPC,        _______, _______, _______,           KC_LEFT, KC_DOWN, KC_RIGHT
+      KC_LCTL,   KC_LALT, _______, KC_LGUI, KC_SPC,          KC_SPC,        _______, _______, _______,           KC_LEFT, KC_DOWN, KC_RIGHT
   ),
 
 };
