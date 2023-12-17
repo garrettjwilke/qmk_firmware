91f03d45169c90ecc279848c71e5ab07fdb1a06d
garrett wilke <garrettwilke@garretts-MacBook-Air.local>
2023-12-17 11:56:56 -0700

    fixed octave up/down

--- keyboards/system76/launch_2/keymaps/midi_piano/keymap.c
+++ keyboards/system76/launch_2/keymaps/midi_piano/keymap.c
@@ -81,11 +81,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_MIDI] = LAYOUT(
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,    _______,
-    _______, MI_Cs, MI_Ds, _______,  MI_Fs, MI_Gs,   MI_As, _______,   MI_Cs1, MI_Ds1,   _______, MI_Fs1, MI_Gs1, MI_As1, _______,
-       MI_C,     MI_D,   MI_E,    MI_F,   MI_G,   MI_A,   MI_B,   MI_C1,   MI_D1,   MI_E1,     MI_F1,  MI_G1,  MI_A1,    MI_B1, _______,
+    _______, MI_Cs, MI_Ds, _______,  MI_Fs, MI_Gs,   MI_As, _______,   MI_Cs1, MI_Ds1,   _______, MI_Fs1, MI_Gs1, MI_As1, MI_OCTU,
+       MI_C,     MI_D,   MI_E,    MI_F,   MI_G,   MI_A,   MI_B,   MI_C1,   MI_D1,   MI_E1,     MI_F1,  MI_G1,  MI_A1,    MI_B1, MI_OCTD,
       _______,     MI_Cs2, MI_Ds2, _______, MI_Fs2, MI_Gs2, MI_As2, _______, MI_Cs3, MI_Ds3, _______, MI_Fs3, MI_Gs3,         _______,
-             MI_C2,   MI_D2,  MI_E2,    MI_F2,  MI_G2,  MI_A2, MI_B2,    MI_C3,  MI_D3,  MI_E3,     MI_F3, MI_G3,          MI_OCTU,
-      _______,   _______, MO(_FUNC), _______, _______,          _______,        _______, _______, _______,         _______, MI_OCTD, _______
+             MI_C2,   MI_D2,  MI_E2,    MI_F2,  MI_G2,  MI_A2, MI_B2,    MI_C3,  MI_D3,  MI_E3,     MI_F3, MI_G3,          _______,
+      _______,   _______, MO(_FUNC), _______, _______,          _______,        _______, _______, _______,         _______, _______, _______
   ),
 
     /* MIDI Pads layer
