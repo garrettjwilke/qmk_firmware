12a201843f03845b249c6327950ecf3f5d8b2d17
garrett wilke <garrettwilke@garretts-MacBook-Air.local>
2023-12-17 13:13:02 -0700

    fixed fn layers

--- keyboards/system76/launch_1/keymaps/midi_ghoti/keymap.c
+++ keyboards/system76/launch_1/keymaps/midi_ghoti/keymap.c
@@ -151,9 +151,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_FUNC] = LAYOUT(
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_BRID, KC_BRIU,              KC_DEL,     _______,
     TO(_BASE), TO(_GHOTI), TO(_PADS), _______,  _______, _______, _______, _______, _______, _______, RGB_TOG, RGB_VAD, RGB_VAI,  KC_BSPC,     KC_VOLU,
-    _______,   _______, _______, _______, KC_R, _______, _______, _______, _______, KC_O,  _______, _______, _______, _______,              KC_VOLD,
-      _______,   KC_A, KC_S, _______, _______, _______, _______, _______, _______,   _______, _______, _______, KC_ENT,         MI_AOFF,
-      KC_LSFT,     KC_Z, KC_X, KC_C, KC_V, _______, _______, _______, _______, _______, _______,       _______,           KC_UP,
+    _______,   KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O,  KC_P, _______, _______, _______,              KC_VOLD,
+      _______,   KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K,   KC_L, _______, _______, KC_ENT,         MI_AOFF,
+      KC_LSFT,     KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, _______, _______, _______,       _______,           KC_UP,
       KC_LCTL,   KC_LALT, _______, KC_LGUI, KC_SPC,          KC_SPC,        _______, _______, _______,           KC_LEFT, KC_DOWN, KC_RIGHT
   ),
 
--- keyboards/system76/launch_lite_1/keymaps/midi_piano/keymap.c
+++ keyboards/system76/launch_lite_1/keymaps/midi_piano/keymap.c
@@ -71,10 +71,10 @@ ________________________________________________________________________________
 */
 
   [_FUNC] = LAYOUT(
-    KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,      KC_MPLY,
-    TO(_BASE), TO(_MIDI), TO(_PADS), _______, KC_R, _______, _______, _______, _______, KC_O,  _______, _______, _______, _______,              KC_VOLD,
-      KC_CAPS,   KC_A, KC_S, _______, _______, _______, _______, _______, _______,   _______, _______, _______, KC_ENT,         MI_AOFF,
-      KC_LSFT,     KC_Z, KC_X, KC_C, KC_V, _______, _______, _______, _______, _______, _______,       _______,           KC_UP, _______,
+    TO(_BASE),  TO(_MIDI),   TO(_PADS),   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,  _______,  _______,  KC_DEL,      _______,
+    _______, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O,  KC_P, _______, _______, _______,              KC_VOLD,
+      _______,   KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K,   KC_L, _______, _______, KC_ENT,         MI_AOFF,
+      KC_LSFT,     KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, _______, _______, _______,       _______,           KC_UP, _______,
       KC_LCTL,   KC_LALT, _______, KC_LGUI, KC_SPC,          KC_SPC,        _______, _______, _______,           KC_LEFT, KC_DOWN, KC_RIGHT
   ),
 
